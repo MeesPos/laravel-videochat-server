b42898335971fa598c8db19ae795e42952e59c26
Mees Postma <35431488+MeesPos@users.noreply.github.com>
2021-02-11 13:27:52 +0100

    Bug with Vue

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::group(['middleware' => 'auth'], function () {
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/vue/{vue_capture?}', function () {
+    return view('vue.index');
+})->where('vue_capture', '[\/\w\.-]*');
