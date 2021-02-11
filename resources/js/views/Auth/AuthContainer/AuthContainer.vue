c5391c71d007bb4ff8619f9fea8f692c9c965f36
LaupWing <loc_strict@hotmail.com>
2021-02-11 13:34:11 +0100

    FIX

--- resources/js/views/Auth/AuthContainer/AuthContainer.vue
+++ resources/js/views/Auth/AuthContainer/AuthContainer.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="max-w-7xl mx-auto w-full h-full bg-white flex flex-col p-5 sm:px-6 lg:px-8">
       <header class="flex items-center">
-         <img src="@/assets/logo.png" class="w-12">
+         <!-- <img src="@/assets/logo.png" class="w-12"> -->
          <p class="ml-3 font-semibold">Seconden</p>
       </header>
       <div class="flex flex-col flex-1">
